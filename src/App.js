9aee8e85d05008c25e25c3137a1dd285301f6b29
Luis Escobedo <71693660+LuisHorus@users.noreply.github.com>
2023-11-04 00:49:18 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './platzi.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <TodoItem></TodoItem>
+      <TodoItem></TodoItem>
+      <TodoItem></TodoItem>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+        </p>
+        <a
+          className="App-link"
+          href="https://platzi.com/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function TodoItem(){
+
+  return(
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+
+export default App;
